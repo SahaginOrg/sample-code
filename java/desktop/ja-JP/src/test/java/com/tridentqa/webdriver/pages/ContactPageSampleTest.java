db38f71ab8734b7fbbe75a8fd5041dc34e7d0cd1
NozomiIto <nozomi.ito@trident-qa.com>
2015-12-12 15:38:52 +0900

    Add Chrome test

--- java/desktop/ja-JP/src/test/java/com/tridentqa/webdriver/pages/ContactPageSampleTest.java
+++ java/desktop/ja-JP/src/test/java/com/tridentqa/webdriver/pages/ContactPageSampleTest.java
@@ -1,19 +1,28 @@
 package com.tridentqa.webdriver.pages;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.sahagin.runlib.external.adapter.webdriver.WebDriverAdapter;
 
+@RunWith(Parameterized.class)
 public class ContactPageSampleTest {
+    private Class<WebDriver> driverClass;
     private WebDriver wd;
     
     @Before
-    public void setUp() {
-        wd = new FirefoxDriver();
+    public void setUp() throws InstantiationException, IllegalAccessException {
+        wd = driverClass.newInstance();
         WebDriverAdapter.setAdapter(wd);
     }
     
@@ -23,6 +32,18 @@ public class ContactPageSampleTest {
             wd.quit();
         }
     }
+
+    @Parameters(name = "{1}")
+    public static Collection<Object[]> testData() {
+        return Arrays.asList(new Object[][]{
+                {FirefoxDriver.class, "Firefox"}, 
+                {ChromeDriver.class, "Chrome"}
+        });
+    }
+    
+    public ContactPageSampleTest(Class<WebDriver> driverClass, String driverName) {
+        this.driverClass = driverClass;
+    }
     
     @Test
     public void 問い合わせが成功すること() {
@@ -31,7 +52,7 @@ public class ContactPageSampleTest {
         wd.findElement(By.name("your-email")).sendKeys("***@***.com");
         wd.findElement(By.name("your-organization")).sendKeys("TRIDENT Inc.");
         wd.findElement(By.name("your-subject")).sendKeys("テスト");
-        wd.findElement(By.name("your-messagef")).sendKeys("テスト送信です");
+        wd.findElement(By.name("your-message")).sendKeys("テスト送信です");
         wd.findElement(By.cssSelector("input.wpcf7-submit")).click();
     }
 }
