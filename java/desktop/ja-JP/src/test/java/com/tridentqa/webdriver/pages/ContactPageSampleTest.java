18bd7755dc488d31cb068cd1b54faf338f3d7316
NozomiIto <nozomi.ito@trident-qa.com>
2015-12-12 16:05:49 +0900

    improve sahagin report

--- java/desktop/ja-JP/src/test/java/com/tridentqa/webdriver/pages/ContactPageSampleTest.java
+++ java/desktop/ja-JP/src/test/java/com/tridentqa/webdriver/pages/ContactPageSampleTest.java
@@ -8,44 +8,38 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.sahagin.runlib.external.CaptureStyle;
+import org.sahagin.runlib.external.TestDoc;
 import org.sahagin.runlib.external.adapter.webdriver.WebDriverAdapter;
 
 public class ContactPageSampleTest {
-        
+
     @Test
     public void Firefoxでの問い合わせが成功すること() {
         WebDriver wd = new FirefoxDriver();
         WebDriverAdapter.setAdapter(wd);
-        try {
-            testMain(wd);
-        } finally {
-            wd.quit();
-        }
+        contact(wd);
+        wd.quit();
     }
-    
+
     @Test
     public void Chromeでの問い合わせが成功すること() {
         WebDriver wd = new ChromeDriver();
         WebDriverAdapter.setAdapter(wd);
-        try {
-            testMain(wd);
-        } finally {
-            wd.quit();
-        }
+        contact(wd);
+        wd.quit();
     }
-    
+
     @Test
     public void PhantomJSでの問い合わせが成功すること() {
         WebDriver wd = new PhantomJSDriver();
         WebDriverAdapter.setAdapter(wd);
-        try {
-            testMain(wd);
-        } finally {
-            wd.quit();
-        }
+        contact(wd);
+        wd.quit();
     }
-    
-    private void testMain(WebDriver wd) {
+
+    @TestDoc(value = "問い合わせを行う", capture=CaptureStyle.STEP_IN)
+    private void contact(WebDriver wd) {
         wd.get("http://www-demo.trident-qa.com/contact/");
         wd.findElement(By.name("your-name")).sendKeys("テストユーザー");
         wd.findElement(By.name("your-email")).sendKeys("***@***.com");
